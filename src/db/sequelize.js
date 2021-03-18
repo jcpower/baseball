57a1f2d9a7e057fa0b7af674a78f7cfe5d8d07dc
jcpower <lasombra@gmail.com>
2021-03-17 22:27:42 -0400

    Update sequelize.js

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,6 +1,10 @@
-const {Sequelize} = require('sequelize')
-const sequelize = new Sequelize('postgres://manager@localhost:5432/baseball')
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize('postgres://manager:RoseHOF14@localhost:5432/baseball')
+
 
 try {
-    sequelize.authenticate().then(() => console.log("Connection has been established successfully.")).catch ((error)=> {
-    console.error('Unable to connect to the database:', error)}
+    sequelize.authenticate().then(() => console.log("Connection has been established successfully."));
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  
