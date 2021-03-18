03199eadacd6e134b13579e4894ddc704674e541
jcpower <lasombra@gmail.com>
2021-03-17 22:05:38 -0400

    Update sequelize.js

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,9 +1,6 @@
 const {Sequelize} = require('sequelize')
-const sequelize = new Sequelize('postgres://user:manager:5432/baseball')
+const sequelize = new Sequelize('postgres://manager@localhost:5432/baseball')
 
 try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+    sequelize.authenticate().then(() => console.log("Connection has been established successfully.")).catch ((error)=> {
+    console.error('Unable to connect to the database:', error)}
