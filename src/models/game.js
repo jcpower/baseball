42db76285ff05768920e999335424bc0b9e3852e
jcpower <lasombra@gmail.com>
2021-03-31 18:16:20 -0400

    timestamp

--- src/models/game.js
+++ src/models/game.js
@@ -1,6 +1,7 @@
 const { Sequelize, DataTypes, Model } = require('sequelize')  //if rerunning sequelize.define example change back to justDataTypes
-const sequelize = new Sequelize('postgres://manager:RoseHOF14@localhost:5432/baseball')
-
+const sequelize = new Sequelize('postgres://manager:RoseHOF14@localhost:5432/baseball',{
+    quoteIdentifiers: false  
+})
 
 try {
     sequelize.authenticate().then(() => console.log("Connection has been established successfully."));
@@ -34,18 +35,25 @@ try {
           type: DataTypes.INTEGER,
           allowNull: false
       },
+      timestamp: {
+          type: DataTypes.TIME  
+      }
+      }, {
       // Other model options go here
       sequelize: sequelize, // We need to pass the connection instance
       modelName: 'Game' // We need to choose the model name
     })
     const testGame = Game.build({
+        date: new Date('2021-04-01'),
         hometeam: "Reds",
-        homteamscore: 10,
+        hometeamscore: 10,
         visitorteam: "Cardinals",
         visitorteamscore: 0
     })
     console.log(testGame instanceof Game)
     console.log(testGame.hometeam) 
+    await testGame.save()
+    console.log('Test Game was saved to the DB!!!!')
 }
   
   testFunction()
