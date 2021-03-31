8b8cf6e2d11275e3f642e81bf58cbb3e6a12c45c
jcpower <lasombra@gmail.com>
2021-03-31 14:22:35 -0400

    working async

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,5 +1,6 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')  //if rerunning sequelize.define example change back to just DataTypes
 const sequelize = new Sequelize('postgres://manager:RoseHOF14@localhost:5432/baseball')
+
 
 
 try {
@@ -7,4 +8,77 @@ try {
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+
+ 
+
+  class User extends Model {}
   
+
+  User.init({
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    }
+  }, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User' // We need to choose the model name
+  })
+  
+  // the defined model is the class itself
+  console.log(User === sequelize.models.User); // true
+
+  await User.sync({ force: true });
+  console.log("The table for the User model was just (re)created!")
+
+
+
+
+  //Practice model -sequelize.define example
+
+  // const User = sequelize.define('User', {
+    //   // Model attributes are defined here
+    //   firstName: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false
+    //   },
+    //   lastName: {
+    //     type: DataTypes.STRING
+    //     // allowNull defaults to true
+    //   }
+    // }, {
+    //   // Other model options go here
+    // });
+    
+    // // `sequelize.define` also returns the model
+    // console.log(User === sequelize.models.User); // true
+
+
+
+
+
+  
+//   const { Sequelize, DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('sqlite::memory:');
+
+// const User = sequelize.define('User', {
+//   // Model attributes are defined here
+//   firstName: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   lastName: {
+//     type: DataTypes.STRING
+//     // allowNull defaults to true
+//   }
+// }, {
+//   // Other model options go here
+// });
+
+// // `sequelize.define` also returns the model
+// console.log(User === sequelize.models.User); // true
